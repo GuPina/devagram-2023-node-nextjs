1f304f65a5bdfcf70f4365ebf8c4a0efc3d98639
Gustavo Pina <115650988+GuPina@users.noreply.github.com>
2023-04-20 21:31:53 -0300

    criptografia, cadastro de usuario e autenticação

--- middlewares/conectarMongoDB.ts
+++ middlewares/conectarMongoDB.ts
@@ -3,23 +3,23 @@ import mongoose from 'mongoose';
 import type {respostaPadraoMsg} from '../types/respostaPadraoMsg';
 
 export const conectarMongoDB = (handler : NextApiHandler) =>
-    async (req: NextApiRequest, res : NextApiResponse<respostaPadraoMsg>) => {
+    async (req: NextApiRequest, res: NextApiResponse<respostaPadraoMsg>) => {
 
         // verificar se o banco já está conectado, se estiver seguir para o endpoint proximo middleware
-        if(mongoose.connections[0].readyState){
+        if (mongoose.connections[0].readyState) {
             return handler(req, res);
         }
 
         // já que não está conectado vamos conectar
         // obter a veriavel de ambiente preenchida do env
-        const {DB_CONEXAO_STRING} = process.env;
+        const { DB_CONEXAO_STRING } = process.env;
 
         // Se a env estiver vazia aborta o uso do sistema e avisa o programador
-        if(!DB_CONEXAO_STRING){
-            return res.status(500).json({ erro : 'ENV de configuração não informado'});
+        if (!DB_CONEXAO_STRING) {
+            return res.status(500).json({ erro: 'ENV de configuração não informado' });
         }
 
-        
+
         mongoose.connection.on('connected', () => console.log('Banco de dados conectado'));
         mongoose.connection.on('error', erro => console.log(`Ocorreu erro ao conectar no banco: ${erro}`));
         await mongoose.connect(DB_CONEXAO_STRING);
--- pages/api/login.ts
+++ pages/api/login.ts
@@ -1,17 +1,20 @@
 import type {NextApiRequest, NextApiResponse} from 'next';
 import { conectarMongoDB } from '../../middlewares/conectarMongoDB';
-import type {respostaPadraoMsg} from '../../types/respostaPadraoMsg'
+import type {respostaPadraoMsg} from '../../types/respostaPadraoMsg';
+import md5 from 'md5';
+import { UsuarioModel } from '../../models/UsuarioModel';
 
-const endpointLogin =  (
+const endpointLogin = async (
     req : NextApiRequest,
     res : NextApiResponse<respostaPadraoMsg>
 ) => {
     if(req.method === 'POST'){
         const {login, senha} = req.body;
 
-        if(login === 'admin@admin.com' &&
-            senha === 'Admin123'){
-               return res.status(200).json({msg : 'Usuario autenticado com sucesso'});
+       const usuarioEncontrados = await UsuarioModel.find({email : login, senha : md5(senha)});
+        if(usuarioEncontrados && usuarioEncontrados.length > 0){
+                const usuarioEncontrado = usuarioEncontrados[0];
+               return res.status(200).json({msg : `Usuario ${usuarioEncontrado.nome} autenticado com sucesso`});
         }
         return res.status(400).json({erro : 'Usuario ou senha invalidos'});
     }
