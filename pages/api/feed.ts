171e5a4f86d7e9f90487d3962e113c64968e29a2
Gustavo Pina <115650988+GuPina@users.noreply.github.com>
2023-05-08 21:56:06 -0300

    update.feed

--- pages/api/feed.ts
+++ pages/api/feed.ts
@@ -4,21 +4,22 @@ import {validarTokenJWT} from '../../middlewares/validarTokenJWT';
 import {conectarMongoDB} from '../../middlewares/conectarMongoDB';
 import { UsuarioModel } from "../../models/UsuarioModel";
 import { PublicacaoModel } from '../../models/publicacaoModel';
+import publicacao from "./publicacao";
 
 const feedEndpoint = async (req : NextApiRequest, res : NextApiResponse<respostaPadraoMsg | any>) => {
     try{
         if(req.method === 'GET'){
-            if(!req?.query?.id){
+            if(req?.query?.id){
                 const usuario = await UsuarioModel.findById(req?.query?.id);
                 if(!usuario){
                     return res.status(400).json({erro : 'Usuario nao encontrado'});
                 }
 
-            const publicacoes = await PublicacaoModel
-                .find({idUsuario : usuario._id})
-                .sort({data : -1});
+                const publicacoes = await PublicacaoModel
+                    .find({idUsuario : usuario._id})
+                    .sort({data : -1});
+                    return res.status(200).json(publicacoes);
             }
-            return res.status(200).json(publicacoes);
         }
         return res.status(405).json({erro : 'Metodo informado não é valido'})
     }catch(e){
